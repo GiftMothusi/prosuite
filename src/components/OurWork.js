d4e38d0c920e6e4f5b399b78075d099019104b2b
ThatoGift <giftgabanakgosi@gmail.com>
2024-02-29 07:47:18 +0200

    final draft

--- src/components/OurWork.js
+++ src/components/OurWork.js
@@ -4,112 +4,86 @@ import GradientButton from "./GradientButton";
 
 const OurWork = () => {
   return (
-    <div className="py-8 container mx-auto lg:h-[80vh] md:h-[200vh] sm:h-[145vh] ss:h-[100vh] xs:h-[160vh]">
-      <div className="items-center justify-center text-left text-[#B5B5B6]">
-        <div className="flex justify-center items-center">
-          <div className="border-r w-[6px] h-[42px] top-30 left-[482] bg-[#91BC4D] mr-2" />
-          <h1 className="text-gradient font-bold text-4xl leading-[96.5px] ">
-            Our{" "}
-            <span className="text-2xl ml-1 font-normal text-gradient">
-              Solutions
-            </span>
-          </h1>
-        </div>
-        {/**GRID LAYER */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {/**CARD 1 */}
-          <div className="">
-            <div className="p-5 flex flex-col">
-              <h5 className="font-sans font-semibold text-2xl leading-4 md:text-xl text-gradient p-4">
-                Asset Management{" "}
-              </h5>
-              <p className="font-sans font-normal leading-6 text-base mt-3">
-                Gain control of your asset with Software that helps you keep
-                track of your physical assets.
-              </p>
-              <div className="font-montserrat py-5">
-                <GradientButton value="LEARN MORE" />
-              </div>
-            </div>
-          </div>
+    <div class="container mx-auto px-4 mb-10">
+      <div className="flex justify-center items-center">
+        <div className="border-r w-[6px] h-[42px] top-30 left-[482] bg-[#91BC4D] mr-2" />
+        <h1 className="text-gradient font-bold text-4xl leading-[96.5px] ">
+          Our{" "}
+          <span className="text-2xl ml-1 font-normal text-gradient">
+            Solutions
+          </span>
+        </h1>
+      </div>
 
-          {/**CARD 2*/}
-          <div className="">
-            <div className="p-5 flex flex-col">
-              <h5 className="font-sans font-semibold text-2xl leading-4 md:text-xl text-gradient p-4">
-                Risk Management
-              </h5>
-              <p className="font-sans font-normal leading-6 text-base mt-3">
-                Identify, capture and monitor enterprise risks with ease.
-              </p>
-              <div
-                className="font-montserrat py-5
-              "
-              >
-                <GradientButton value="LEARN MORE" />
-              </div>
-            </div>
-          </div>
-          {/**CARD 3 */}
-          <div className="">
-            <div className="p-5 flex flex-col">
-              <h5 className="font-sans font-semibold text-2xl leading-4 md:text-xl text-gradient p-4">
-                Documents Management
-              </h5>
-              <p className="font-sans font-normal leading-6 text-base mt-3">
-                Easy-to-use solution that enables your firm to convert paper
-                into electronic files.
-              </p>
-              <div className="font-montserrat py-5">
-                <GradientButton value="LEARN MORE" />
-              </div>
-            </div>
-          </div>
-          {/**CARD 4 */}
-          <div className="">
-            <div className="p-5 flex flex-col">
-              <h5 className="font-sans font-semibold text-2xl leading-4 md:text-xl text-gradient p-4">
-                Perfomance Management
-              </h5>
-              <p className="font-sans font-normal leading-6 text-base mt-3">
-                Achieve business stragetic goals with a solution ensuring
-                organizations are running effenciently
-              </p>
-              <div className="font-montserrat py-5">
-                <GradientButton value="LEARN MORE" />
-              </div>
-            </div>
-          </div>
-          {/**CARD 5 */}
-          <div className="">
-            <div className="p-5 flex flex-col">
-              <h5 className="font-sans font-semibold text-2xl leading-4 md:text-xl text-gradient p-4">
-                Audit
-              </h5>
-              <p className="font-sans font-normal leading-6 text-base mt-3">
-                Streamline your audit processes and comply with regulations or
-                internal policies.
-              </p>
-              <div className="font-montserrat py-5">
-                <GradientButton value="LEARN MORE" />
-              </div>
-            </div>
-          </div>
-          {/**CARD 6 */}
-          <div className="">
-            <div className="p-5 flex flex-col">
-              <h5 className="font-sans font-semibold text-2xl leading-4 md:text-xl text-gradient p-4">
-                Compliance Management
-              </h5>
-              <p className="font-sans font-normal leading-6 text-base mt-3">
-                Manage and meet legal requirements and comply with applicable
-                laws and regulations.
-              </p>
-              <div className="font-montserrat py-5">
-                <GradientButton value="LEARN MORE" />
-              </div>
-            </div>
-          </div>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {/*<!-- Grid Item 1-->*/}
+        <div class="p-4 rounded-lg">
+          <h1 class="font-sans font-semibold text-2xl leading-4 text-gradient mb-2 pb-2 pt-2">
+            Asset Management
+          </h1>
+          <p class="font-sans font-normal leading-6 text-base mb-4 text-[#B5B5B6]">
+            Gain control of your asset with Software that helps you keep track
+            of your physical assets.
+          </p>
+          <GradientButton value="LEARN MORE" />
+        </div>
+
+        {/*<!-- Grid Item 2 -->*/}
+        <div class=" p-4 rounded-lg">
+          <h1 class="font-sans font-semibold text-2xl leading-4 text-gradient mb-2 pb-2 pt-2">
+            Risk Management
+          </h1>
+          <p class="font-sans font-normal leading-6 text-base mb-4 text-[#B5B5B6]">
+            Identify, capture and monitor enterprise risks with ease.
+          </p>
+          <GradientButton value="LEARN MORE" />
+        </div>
+
+        {/*<!-- Grid Item 3-->*/}
+        <div class=" p-4 rounded-lg">
+          <h1 class="font-sans font-semibold text-2xl leading-4 text-gradient mb-2 pb-2 pt-2">
+            Documents Management
+          </h1>
+          <p class="font-sans font-normal leading-6 text-base mb-4 text-[#B5B5B6]">
+            Easy-to-use solution that enables your firm to convert paper into
+            electronic files.
+          </p>
+          <GradientButton value="LEARN MORE" />
+        </div>
+
+        {/*<!-- Grid Item 4-->*/}
+        <div class=" p-4 rounded-lg">
+          <h1 class="font-sans font-semibold text-2xl leading-4 text-gradient mb-2 pb-2 pt-2">
+            Perfomance Management
+          </h1>
+          <p class="font-sans font-normal leading-6 text-base mb-4 text-[#B5B5B6]">
+            Achieve business stragetic goals with a solution ensuring
+            organizations are running effenciently
+          </p>
+          <GradientButton value="LEARN MORE" />
+        </div>
+        {/*<!-- Grid Item 5 -->*/}
+        <div class=" p-4 rounded-lg">
+          <h1 class="font-sans font-semibold text-2xl leading-4 text-gradient mb-2 pb-2 pt-2">
+            Audit
+          </h1>
+          <p class="font-sans font-normal leading-6 text-base mb-4 text-[#B5B5B6]">
+            Streamline your audit processes and comply with regulations or
+            internal policies.
+          </p>
+          <GradientButton value="LEARN MORE" />
+        </div>
+
+        {/*<!-- Grid Item 6 -->*/}
+        <div class=" p-4 rounded-lg">
+          <h1 class="font-sans font-semibold text-2xl leading-4 text-gradient mb-2 pb-2 pt-2">
+            Compliance Management
+          </h1>
+          <p class="font-sans font-normal leading-6 text-base mb-4 text-[#B5B5B6]">
+            Manage and meet legal requirements and comply with applicable laws
+            and regulations.
+          </p>
+          <GradientButton value="LEARN MORE" />
         </div>
       </div>
     </div>
